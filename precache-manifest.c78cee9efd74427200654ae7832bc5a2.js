a87fdbc3660ec061cf4eb7325d2b9ba4a3c8dd36
kace-study <kace.study@gmail.com>
2018-11-01 12:21:04 +0900

    Updates

---
+++ precache-manifest.c78cee9efd74427200654ae7832bc5a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "622c852619ba2f1c20cf",
+    "url": "/react-all-blacks/static/js/runtime~main.622c8526.js"
+  },
+  {
+    "revision": "01fc0f6fc68e23a221cf",
+    "url": "/react-all-blacks/static/js/main.01fc0f6f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-all-blacks/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "01fc0f6fc68e23a221cf",
+    "url": "/react-all-blacks/static/css/main.dd92256f.chunk.css"
+  },
+  {
+    "revision": "e3a9975751f39160a0c8ec5567adaa4d",
+    "url": "/react-all-blacks/index.html"
+  }
+];
